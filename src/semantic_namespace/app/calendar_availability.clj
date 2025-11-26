a90f29c9f57da51b798158cc327c30f2c4d43fe9
Juan Antonio Ruz Velasco <juanantonioruz@gmail.com>
2025-11-26 19:30:46 +0100

    foo

--- src/semantic_namespace/app/calendar_availability.clj
+++ src/semantic_namespace/app/calendar_availability.clj
@@ -3,254 +3,99 @@
    [semantic-namespace.compound.identity :as cid]))
 
 (defn init-registry!
-  "Initialize the calendar availability registry with compound identities."
+  "Minimal semantic registry for solo dev with OAuth support."
   []
   ;; Data Schemas
   (cid/register!
-   :schema.data/users
-   #{:semantic-namespace/data-schema
-     :semantic/docs
-     :domain/users}
-   {::fields [:user/id :user/email :user/language :user/gcal-refresh-token]
-    :docs/content "User data schema with identity, contact, language, and Google Calendar token."})
+   :schema/users
+   #{:semantic-namespace/data-schema :domain/users}
+   {::fields [:user/id :user/email :user/language :user/gcal-refresh-token]})
 
   (cid/register!
-   :schema.data.scheduling/query
-   #{:semantic-namespace/data-schema
-     :semantic/docs
-     :domain/scheduling
-     :scheduling/query}
-   {::fields [:query/date :query/language]
-    :docs/content "Scheduling query input: target date and language filter."})
+   :schema/scheduling-query
+   #{:semantic-namespace/data-schema :domain/scheduling}
+   {::fields [:query/date :query/language]})
 
   (cid/register!
-   :schema.data.scheduling/availability
-   #{:semantic-namespace/data-schema
-     :semantic/docs
-     :domain/scheduling
-     :scheduling/availability}
-   {::fields [:availability/users]
-    :docs/content "Availability result: list of available users."})
+   :schema/availability
+   #{:semantic-namespace/data-schema :domain/scheduling}
+   {::fields [:availability/users]})
+
+  ;; Components
+  (cid/register!
+   :component/db
+   #{:semantic-namespace/component :tier/foundation :domain/users}
+   {::deps #{}})
 
   (cid/register!
-   :schema.data.google/calendar-event
-   #{:semantic-namespace/data-schema
-     :semantic/docs
-     :domain/google
-     :google/calendar-event}
-   {::fields [:gcal.event/start :gcal.event/end]
-    :docs/content "Google Calendar event with start/end timestamps."})
-
-  ;; Foundation Components
-  (cid/register!
-   :component.foundation.users/db
-   #{:semantic-namespace/component
-     :semantic/docs
-     :tier/foundation
-     :domain/users
-     :integration/internal
-     :effect/read
-     :observability/metered}
-   {::deps #{}
-    ::impl ::not-implemented
-    :docs/content "Database component for user persistence."})
+   :component/google-oauth
+   #{:semantic-namespace/component :tier/foundation :domain/google :integration/external :protocol/oauth}
+   {::deps #{}})
 
   (cid/register!
-   :component.foundation.google/oauth
-   #{:semantic-namespace/component
-     :semantic/docs
-     :tier/foundation
-     :domain/google
-     :integration/external
-     :protocol/http
-     :protocol/oauth
-     :effect/read
-     :temporal/async
-     :temporal/timeout-configured
-     :observability/traced}
-   {::deps #{}
-    ::impl ::not-implemented
-    :docs/content "Google OAuth2 component for API authorization."})
+   :component/gcal-client
+   #{:semantic-namespace/component :tier/foundation :domain/google :integration/external}
+   {::deps #{:component/google-oauth}})
 
+  ;; Functions
   (cid/register!
-   :component.foundation.google/gcal-http
-   #{:semantic-namespace/component
-     :semantic/docs
-     :tier/foundation
-     :domain/google
-     :domain/calendar
-     :integration/external
-     :protocol/http
-     :effect/read
-     :temporal/async
-     :temporal/timeout-configured
-     :observability/traced}
-   {::deps #{}
-    ::impl ::not-implemented
-    :docs/content "Google Calendar HTTP client for event retrieval."})
-
-  ;; Service Functions
-  (cid/register!
-   :service.users/find-by-language
-   #{:semantic-namespace/function
-     :semantic/docs
-     :tier/service
-     :domain/users
-     :operation/search
-     :effect/read
-     :observability/metered}
+   :fn/find-users-by-language
+   #{:semantic-namespace/function :tier/service :domain/users}
    {::context [:query/language]
     ::response [:user/email :user/gcal-refresh-token]
-    ::deps #{:component.foundation.users/db}
-    ::impl ::not-implemented
-    :docs/content "Search users by language preference."})
+    ::deps #{:component/db}})
 
   (cid/register!
-   :service.scheduling/check-user-day
-   #{:semantic-namespace/function
-     :semantic/docs
-     :tier/service
-     :domain/scheduling
-     :operation/validate
-     :effect/read
-     :integration/external
-     :temporal/async
-     :temporal/timeout-configured
-     :observability/traced
-     :compliance/pii
-     :compliance/audited}
-   {::context [:query/date :user/gcal-refresh-token]
+   :fn/refresh-oauth-token
+   #{:semantic-namespace/function :tier/service :domain/google :integration/external :protocol/oauth}
+   {::context [:user/gcal-refresh-token]
+    ::response [:oauth/access-token]
+    ::deps #{:component/google-oauth}})
+
+  (cid/register!
+   :fn/check-user-availability
+   #{:semantic-namespace/function :tier/service :domain/scheduling :integration/external}
+   {::context [:query/date :oauth/access-token]
     ::response [:scheduling/available?]
-    ::deps #{:component.foundation.google/gcal-http}
-    ::impl ::not-implemented
-    :docs/content "Check user availability for a date via Google Calendar."})
+    ::deps #{:component/gcal-client}})
 
   (cid/register!
-   :service.scheduling/collect-free-users
-   #{:semantic-namespace/function
-     :semantic/docs
-     :tier/service
-     :domain/scheduling
-     :operation/process
-     :effect/pure
-     :observability/metered}
+   :fn/collect-available-users
+   #{:semantic-namespace/function :tier/service :domain/scheduling :effect/pure}
    {::context [:query/date :query/language :user/email]
     ::response [:availability/users]
-    ::deps #{}
-    ::impl ::not-implemented
-    :docs/content "Aggregate available users into final result."})
+    ::deps #{}})
 
-  ;; API Endpoint
+  ;; Endpoint
   (cid/register!
-   :endpoint.api.scheduling/query-availability
-   #{:semantic-namespace/endpoint
-     :semantic-namespace/function
-     :semantic/docs
-     :tier/api
-     :domain/scheduling
-     :operation/search
-     :effect/read
-     :protocol/http
-     :authorization/required
-     :observability/logged
-     :observability/metered
-     :capacity/rate-limited
-     :compliance/pii
-     :compliance/audited}
+   :endpoint/query-availability
+   #{:semantic-namespace/endpoint :semantic-namespace/function :tier/api :domain/scheduling}
    {::context [:query/date :query/language]
     ::response [:availability/users]
-    ::deps #{:service.users/find-by-language
-             :service.scheduling/check-user-day
-             :service.scheduling/collect-free-users}
-    ::impl ::not-implemented
-    :docs/content "HTTP endpoint for querying user availability."})
+    ::deps #{:fn/find-users-by-language
+             :fn/refresh-oauth-token
+             :fn/check-user-availability
+             :fn/collect-available-users}}))
 
-  ;; Error Handlers
-  (cid/register!
-   :handler.error.http/google
-   #{:semantic-namespace/error-handler
-     :semantic/docs
-     :domain/google
-     :protocol/http}
-   {::impl ::not-implemented
-    :docs/content "Handle Google API HTTP failures."})
-
-  (cid/register!
-   :handler.error.timeout/scheduling
-   #{:semantic-namespace/error-handler
-     :semantic/docs
-     :domain/scheduling
-     :temporal/timeout}
-   {::impl ::not-implemented
-    :docs/content "Handle scheduling timeout errors."})
-
-  (cid/register!
-   :handler.error.auth/unauthorized
-   #{:semantic-namespace/error-handler
-     :semantic/docs
-     :domain/api
-     :authorization/unauthorized}
-   {::impl ::not-implemented
-    :docs/content "Handle unauthorized API requests."})
-
-  (cid/register!
-   :handler.error.capacity/rate-limited
-   #{:semantic-namespace/error-handler
-     :semantic/docs
-     :domain/api
-     :capacity/rate-limited}
-   {::impl ::not-implemented
-    :docs/content "Handle rate limit exceeded errors."}))
-
-(defn function-identity?
-  [id]
+(defn function-identity? [id]
   (contains? id :semantic-namespace/function))
 
-(defn endpoint-identity?
-  [id]
+(defn endpoint-identity? [id]
   (contains? id :semantic-namespace/endpoint))
 
-(defn external-integration?
-  [id]
+(defn external-integration? [id]
   (contains? id :integration/external))
 
-(defn async?
-  [id]
-  (contains? id :temporal/async))
-
-(defn axiom-function-has-impl
-  [[id v]]
-  (when (function-identity? id)
-    (when-not (contains? v ::impl)
-      {:axiom ::function-has-impl
-       :identity id
-       :reason "Function identity missing ::impl key"})))
-
-(defn axiom-external-must-be-async
-  [[id _v]]
-  (when (external-integration? id)
-    (when-not (async? id)
-      {:axiom ::external-must-be-async
-       :identity id
-       :reason "External integration should be :temporal/async"})))
-
-(defn axiom-endpoint-is-function
-  [[id _v]]
+(defn axiom-endpoint-is-function [[id _v]]
   (when (endpoint-identity? id)
     (when-not (function-identity? id)
       {:axiom ::endpoint-is-function
        :identity id
-       :reason "Endpoint identity should also include :semantic-namespace/function"})))
+       :reason "Endpoint must include :semantic-namespace/function"})))
 
-(def axioms
-  [axiom-function-has-impl
-   axiom-external-must-be-async
-   axiom-endpoint-is-function])
+(def axioms [axiom-endpoint-is-function])
 
-(defn check-axioms
-  "Run all axioms over the registry.
-   Returns a seq of violation maps."
-  []
+(defn check-axioms []
   (for [entry @cid/registry
         axiom axioms
         :let [violation (axiom entry)]
